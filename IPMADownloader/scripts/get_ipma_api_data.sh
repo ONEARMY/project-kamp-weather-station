69cf8094d2b9dd874f810a4c663bb607ebab3305
JScheffer <56606926+JoostScheffer@users.noreply.github.com>
2021-04-07 20:49:07 +0200

    added log functionality

--- IPMADownloader/scripts/get_ipma_api_data.sh
+++ IPMADownloader/scripts/get_ipma_api_data.sh
@@ -6,8 +6,8 @@ if [ ! -d ~/WeatherStation/IPMADownloader/data ]; then
   mkdir -p ~/WeatherStation/IPMADownloader/data;
 fi
 
-python3 download_json.py
-python3 download_geojson.py
+python3 download_json.py >> ../json_log.txt
+python3 download_geojson.py >> ../geojson_log.txt
 
 # write date to file so that a malfunction can be located if needed
 echo $(date) >> ../cron.txt
