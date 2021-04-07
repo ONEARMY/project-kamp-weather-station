315a499363c456cec1cf47477400de21897353fd
JScheffer <56606926+JoostScheffer@users.noreply.github.com>
2021-04-07 20:43:55 +0200

    add function to make a data folder if it doesn't exist yet

--- IPMADownloader/scripts/get_ipma_api_data.sh
+++ IPMADownloader/scripts/get_ipma_api_data.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cd ~/WeatherStation/IPMADownloader/scripts/
+
+# if there is no folder for the data yet make one
+if [ ! -d ~/WeatherStation/IPMADownloader/data ]; then
+  mkdir -p ~/WeatherStation/IPMADownloader/data;
+fi
+
 python3 download_json.py
 python3 download_geojson.py
 
